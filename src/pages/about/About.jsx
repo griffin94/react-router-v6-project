d12e68b1fcd2a863752a33919555cdbac3015e1e
Kamil Komarowski <k.w.komarowski@gmail.com>
2023-07-20 22:53:17 +0200

    About page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import cs from './About.module.scss';
+import cx from 'classnames';
+import heroImg from '../../../public/hero_about.png';
+import { Hero } from '../../components';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={cx(cs.about)}>
+      <Hero
+        className={cx(cs.hero)}
+        img={heroImg}
+      />
+      <section className={cx(cs.section)}>
+        <h1 className={cx(cs.section__title)}>
+          Don’t squeeze in a sedan when you could relax in a van.
+        </h1>
+        <p className={cx(cs.section__paragraph)}>
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉)
+        </p>
+        <p className={cx(cs.section__paragraph)}>
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+        <div className={cx(cs.box)}>
+          <p className={cx(cs.box__paragraph)}>
+            Your destination is waiting. Your van is ready.
+          </p>
+          <button className={cx(cs.box__button)}>Explore our vans</button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default About;
