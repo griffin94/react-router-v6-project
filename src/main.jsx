49070a5e6c8f4fb22cac72d5ef27127c87e56128
Kamil Komarowski <k.w.komarowski@gmail.com>
2023-07-24 21:20:03 +0200

    Mirage JS Server

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import router from './pages';
 import './main.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './mocks/server';
 
 ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
