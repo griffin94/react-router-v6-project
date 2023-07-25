0b4917e984143cdf06ad7519254d75890ca30bcc
Kamil Komarowski <k.w.komarowski@gmail.com>
2023-07-25 21:35:01 +0200

    ProductList component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Footer } from './Footer/Footer';
 export { default as Hero } from './Hero/Hero';
 export { default as Header } from './Header/Header';
 export { default as Nav } from './Nav/Nav';
+export { default as ProductList } from './ProductList/ProductList';
