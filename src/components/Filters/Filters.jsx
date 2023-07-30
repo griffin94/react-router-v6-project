8a4ebeebc0604fa54f950a146ade9a7b52999ff0
Kamil Komarowski <k.w.komarowski@gmail.com>
2023-07-30 22:53:36 +0200

    Filters component

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,55 @@
+import { Children, cloneElement, useState } from 'react';
+import cs from './Filters.module.scss';
+import cx from 'classnames';
+
+const Filters = ({ children, onSetFilter }) => {
+  const [activeFilters, setActiveFilters] = useState([]);
+
+  const clearFilters = () =>
+    setActiveFilters(() => {
+      const nextFilters = [];
+      if (typeof onSetFilter === 'function') onSetFilter(nextFilters);
+      return nextFilters;
+    });
+
+  const setFilters = (e) =>
+    setActiveFilters((currentFilters) => {
+      const clickedFilter = e.target.name;
+      const nextFilters = currentFilters.includes(clickedFilter)
+        ? currentFilters.filter((filter) => filter !== clickedFilter)
+        : [...currentFilters, clickedFilter];
+      if (typeof onSetFilter === 'function') onSetFilter(nextFilters);
+      return nextFilters;
+    });
+
+  return (
+    <div className={cx(cs.filters)}>
+      {Children.map(children, (child) =>
+        cloneElement(child, {
+          onClick: setFilters,
+          isActive: activeFilters.includes(child.props.name),
+        })
+      )}
+      <button
+        className={cx(cs.resetButton)}
+        onClick={clearFilters}
+      >
+        Clear filters
+      </button>
+    </div>
+  );
+};
+
+const Button = ({ children, isActive, name, onClick }) => (
+  <button
+    className={cx(cs.button, isActive && cs.active)}
+    name={name}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+export default Object.assign(Filters, {
+  Button,
+});
