a2378afe450181a025a58bdff16b87abb7e12ea7
Kamil Komarowski <k.w.komarowski@gmail.com>
2023-09-09 18:00:46 +0200

    Reviews page pagination

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -10,17 +10,37 @@ export const handlers = [
     );
     return res(ctx.delay(2000), ctx.status(200), ctx.json(data));
   }),
+
   rest.get('/vans/:id', (req, res, ctx) => {
     const id = req.params.id;
     const data = vans.find((van) => van.id === id);
     return res(ctx.delay(2000), ctx.status(200), ctx.json(data));
   }),
+
   rest.get('/host/reviews', (req, res, ctx) => {
-    return res(ctx.delay(500), ctx.status(200), ctx.json(reviews));
+    const REVIEWS_PER_PAGE = 5;
+    const activePage = Number(req.url.searchParams.get('page') || 1);
+    const reviewsAmount = reviews.length;
+    const pagesAmount = Math.ceil(reviewsAmount / REVIEWS_PER_PAGE);
+    const data = reviews.reduce((result, review, idx) => {
+      const startIndex = (activePage - 1) * REVIEWS_PER_PAGE;
+      const endIndex = startIndex + REVIEWS_PER_PAGE - 1;
+      return [
+        ...result,
+        ...(idx >= startIndex && idx <= endIndex ? [review] : []),
+      ];
+    }, []);
+    return res(
+      ctx.delay(500),
+      ctx.status(200),
+      ctx.json({ activePage, pagesAmount, reviews: data, reviewsAmount })
+    );
   }),
+
   rest.get('/host/vans', (req, res, ctx) => {
     return res(ctx.delay(500), ctx.status(200), ctx.json(vans));
   }),
+
   rest.get('/host/vans/:id', (req, res, ctx) => {
     const id = req.params.id;
     const data = vans.find((van) => van.id === id);
--- src/pages/host/HostReviews.jsx
+++ src/pages/host/HostReviews.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Await, useLoaderData } from 'react-router-dom';
-import { Stack } from 'react-bootstrap';
+import { Await, useLoaderData, useSearchParams } from 'react-router-dom';
+import { Pagination, Stack } from 'react-bootstrap';
 import { Image, Rating } from '@/components';
 import Graph from '/income-graph.png';
 
@@ -19,23 +19,63 @@ const Review = ({ date, name, rating, text }) => (
 );
 
 const HostReviews = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const { data } = useLoaderData();
+
+  const setPrevPage = () =>
+    setSearchParams((currentSearchParams) => {
+      const currentPage = Number(currentSearchParams.get('page') || 1);
+      if (currentPage > 1) {
+        currentSearchParams.set('page', currentPage - 1);
+      }
+      return currentSearchParams;
+    });
+
+  const setNextPage = (pages) =>
+    setSearchParams((currentSearchParams) => {
+      const currentPage = Number(currentSearchParams.get('page') || 1);
+      if (currentPage < pages) {
+        currentSearchParams.set('page', currentPage + 1);
+      }
+      return currentSearchParams;
+    });
+
+  const setPage = (page) =>
+    setSearchParams((currentSearchParams) => {
+      currentSearchParams.set('page', page);
+      return currentSearchParams;
+    });
+
   return (
-    <React.Suspense>
+    <React.Suspense fallback={<h1>Loading...</h1>}>
       <Await resolve={data}>
-        {(reviews) => (
+        {({ activePage, pagesAmount, reviews, reviewsAmount }) => (
           <Stack gap={4}>
             <Image
               src={Graph}
               style={{ maxWidth: '400px' }}
             />
-            <span className='fw-bold'>{`Reviews (${reviews.length})`}</span>
+            <span className='fw-bold'>{`Reviews (${reviewsAmount})`}</span>
             {reviews.map((review) => (
               <Review
                 key={review.id}
                 {...review}
               />
             ))}
+            <Pagination className='mx-auto'>
+              <Pagination.First onClick={() => setPage(1)} />
+              <Pagination.Prev onClick={setPrevPage} />
+              {[...Array(pagesAmount).keys()].map((page) => (
+                <Pagination.Item
+                  active={page + 1 === activePage}
+                  onClick={(e) => setPage(e.target.textContent)}
+                >
+                  {page + 1}
+                </Pagination.Item>
+              ))}
+              <Pagination.Next onClick={() => setNextPage(pagesAmount)} />
+              <Pagination.Last onClick={() => setPage(pagesAmount)} />
+            </Pagination>
           </Stack>
         )}
       </Await>
