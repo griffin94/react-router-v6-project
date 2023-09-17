56dbfd77041a5f600a576b04b354499aafa8ac9b
Kamil Komarowski <k.w.komarowski@gmail.com>
2023-09-17 15:42:37 +0200

    Fixed problem with passing additional className in Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,12 +4,13 @@ import cs from './Button.module.scss';
 const Button = ({
   as: Component = 'button',
   children,
+  className,
   isActive,
   variant,
   ...props
 }) => (
   <Component
-    className={cx(cs.button, cs[variant], isActive && cs.active)}
+    className={cx(cs.button, cs[variant], isActive && cs.active, className)}
     {...props}
   >
     {children}
